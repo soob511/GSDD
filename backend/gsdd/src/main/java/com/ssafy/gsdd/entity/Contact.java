5846057316b721060e988c211abda9d43b2d8da4
오정환 <wjdghks6086@naver.com>
2022-09-28 16:42:38 +0900

    [feat] BE : merge 후 삭제파일 복원

---
+++ backend/gsdd/src/main/java/com/ssafy/gsdd/entity/Contact.java
@@ -0,0 +1,47 @@
+package com.ssafy.gsdd.entity;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@NoArgsConstructor
+@Getter @Setter
+@ToString(exclude = "user")
+public class Contact {
+
+    @Id
+    @GeneratedValue
+    private int contactId;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_Id")
+    private User user;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String contact;
+
+    @Column(nullable = false)
+    private LocalDateTime regDate;
+
+    public Contact(String name, String contact, LocalDateTime regDate, User user) {
+        this.name = name;
+        this.contact = contact;
+        this.regDate = regDate;
+        this.addContact(user);
+    }
+
+    public void addContact(User user){
+        this.user = user;
+        user.getContacts().add(this);
+    }
+
+
+}
--- backend/gsdd/src/main/java/com/ssafy/gsdd/repository/UserRepository.java
+++ backend/gsdd/src/main/java/com/ssafy/gsdd/repository/UserRepository.java
@@ -1,6 +1,7 @@
 package com.ssafy.gsdd.repository;
 
 import com.ssafy.gsdd.entity.User;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -8,6 +9,11 @@ import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User,Integer> {
+
+
+    @EntityGraph(attributePaths = {"contacts"})
+    User findByUserId(int id);
+
 
     Optional<User> findByEmail(String email);
 
