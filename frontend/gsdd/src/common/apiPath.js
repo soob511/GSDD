5f08e36935fc740f08de974b1253c2b27a085b17
오정환 <wjdghks6086@naver.com>
2022-09-28 13:00:11 +0900

    [feat] FE : axios 주소 수정

--- frontend/gsdd/src/common/apiPath.js
+++ frontend/gsdd/src/common/apiPath.js
@@ -1,16 +1,16 @@
-const HOST = 'https://j7b209.p.ssafy.io:80/api/';
+const HOST = "http://j7b209.p.ssafy.io:80/api/";
 
-const AUTH = 'auth/';
-const USER = 'user/';
-const MARKERS = 'markers/';
-const MYPAGE = 'mypage/';
-const ROUTE = 'route/';
-const CONTACT = 'contact/';
+const AUTH = "auth/";
+const USER = "user/";
+const MARKERS = "markers/";
+const MYPAGE = "mypage/";
+const ROUTE = "route/";
+const CONTACT = "contact/";
 
 const apiPath = {
   auth: {
-    login: () => HOST + AUTH + 'login/',
-    logout: () => HOST + AUTH + 'logout/',
+    login: () => HOST + AUTH + "login/",
+    logout: () => HOST + AUTH + "logout/",
   },
   markers: {
     get: (lat, lng) => HOST + MARKERS + `${lat}/` + `${lng}/`,
